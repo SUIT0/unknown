8ebd1c394cf71081e0dabd11fc1315ce8947dadc
SUITO <sudipacharya9841647628@gmail.com>
2024-09-13 04:42:39 +0545

    yay2

--- python.py
+++ python.py
@@ -1,1 +1,13 @@
 print ("hello world")
+
+b="sudip"
+
+for i in range (5):print("hello world")
+
+def ulala (n): 
+    for i in range (100):
+        print(f"{n}")
+        
+       
+b="sudip"       
+ulala(b)
